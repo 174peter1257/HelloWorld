c19a4080114a9f3607c9b2260274d6ec5e1dcd0d
peter <peter@peterde-MacBook-Pro.local>
2020-02-09 02:51:32 -0500

    add new println

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package helloworld;
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Just want to have some fun!! => ");
+        System.out.println("still confusing");
     }
 }
