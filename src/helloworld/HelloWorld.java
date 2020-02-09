fff53352b675fd56e2380fc561fc5923bf943a5d
unknown <174peter1257@gmail.com>
2020-02-09 02:21:02 -0500

    first commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Just want to have some fun!! => ");
+    }
+}
