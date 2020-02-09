e725d51d40ec5faba85ea068af3bdcaefdcb5a25
peter <peter@peterde-MacBook-Pro.local>
2020-02-09 16:00:30 -0500

    new println!

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
         System.out.println("Just want to have some fun!! => ");
 
         System.out.println("ocean harbor is so delicious!!!");
+
+        System.out.println("the weather today is so good!");
     }
 } 
